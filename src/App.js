c6a5fce381060f0011565a8be05745f8e359dc18
ch-ratnaji <ratnaji.it91@gmail.com>
2024-05-30 23:40:31 +0530

    NavLink replaced link in MainNavigation component add added className attribute to it but it takes a function instead of string and it recieves an object and we can destructure isActive property from it.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./components/Root";
+import Errorpage from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <Errorpage />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
---
+++ src/pages/Error.js
@@ -0,0 +1,15 @@
+import MainNavigation from "../components/MainNavigation";
+
+function Errorpage() {
+    return (
+        <>
+            <MainNavigation />
+            <main>
+                <h1>An error occured!</h1>
+                <p>Couldn't find this page</p>
+            </main>
+        </>
+    );
+}
+
+export default Errorpage;
