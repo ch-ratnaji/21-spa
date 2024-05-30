a657ed5de41e766271a24d40b36832deeee940bd
ch-ratnaji <ratnaji.it91@gmail.com>
2024-05-31 00:13:21 +0530

    Here in App component i have added another route in children. In that path /:productId could be anything the output will be same i.e <ProductDetailPage/>

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,3 +1,12 @@
 export default function ProductsPage() {
-    return (<h1>Products page </h1>);
+    return (
+        <>
+            <h1>Products page </h1>
+            <ul>
+                <li>Product1</li>
+                <li>Product2</li>
+                <li>Product3</li>
+            </ul>
+        </>
+    );
 }
