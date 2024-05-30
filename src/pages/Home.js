d5aeb2f809b513f89b6a6c0eff72a6f6e94dcb94
ch-ratnaji <ratnaji.it91@gmail.com>
2024-05-30 15:31:49 +0530

    Here in this we dont face the problem i.e sending http request when we click on list of products link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
     return (
         <>
             <h1>My home page</h1>
             <p>
-                Go to <a href="/products">list of products</a>.
+                Go to <Link to="/products">list of products</Link>.
             </p>
         </>
     );
