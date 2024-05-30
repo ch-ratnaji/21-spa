1ad839a5a7ed5ddda8c8cb1bcf28c372150fbc36
ch-ratnaji <ratnaji.it91@gmail.com>
2024-05-30 15:24:15 +0530

    Here in this example it seems there is no problem but when we click the products link it we can observe refresh symbol blinks once i.e it sends http request whenever we click that link therfore all javascript or react code have to load for every click on that link and at the same time we may lost the data on context state or app wide state. These are all risks with this approach

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+export default function HomePage() {
+    return (
+        <>
+            <h1>My home page</h1>
+            <p>
+                Go to <a href="/products">list of products</a>.
+            </p>
+        </>
+    );
+}
